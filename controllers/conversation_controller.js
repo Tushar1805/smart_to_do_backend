e2d597b039b7be38511c207e97ea3cc42650e410
Tushar Kalbhande <tusharkalbhande1805@gmail.com>
2025-04-10 08:56:57 +0530

    conversation response update

--- controllers/conversation_controller.js
+++ controllers/conversation_controller.js
@@ -7,7 +7,9 @@ const Conversations = require("../models/conversation_model");
 const getConversation = asyncHandler(async (req, res) => {
   //   const contacts = await Conversations.find({ user_id: req.user.id });
   const contacts = await Conversations.find();
-  res.status(200).json(contacts);
+  res
+    .status(200)
+    .json({ data: contacts, message: "conversations fetched successfully" });
 });
 
 //@desc Create Conversation
@@ -15,7 +17,7 @@ const getConversation = asyncHandler(async (req, res) => {
 //@access private
 const createConversation = asyncHandler(async (req, res) => {
   console.log("The request body is ", req.body);
-  const { title, messages} = req.body;
+  const { title, messages } = req.body;
   if (!messages) {
     res.status(400);
     throw new Error("All fields are mandatory");
